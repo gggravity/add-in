e93f684a3e807d72629a590d151f57e0a021bc81
Martin Eriksen <m@rtin.bz>
2023-01-25 14:59:31 -0300

    .

---
+++ main.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+path = "L:/RASTER_DATA/"
+ext = "*.ecw"
+
+for path in Path(path).rglob(ext):
+    code = path.absolute().__str__().split("\\")[2]
+    year = path.absolute().__str__().split("\\")[3]
+
+    coords = path.name.split("_")[-1].split(".")[0]
+    if coords.startswith("Z"):
+        print(path.absolute().__str__() + "|" + code + " " + year + " " + coords)
+    else:
+        print(path.absolute().__str__() + "|" + code + " " + year)
+
