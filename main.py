fbe941db88821f37a5b433cf2962693486e3f44c
Martin Eriksen <m@rtin.bz>
2023-01-25 15:00:41 -0300

    .

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ ext = "*.ecw"
 for path in Path(path).rglob(ext):
     code = path.absolute().__str__().split("\\")[2]
     year = path.absolute().__str__().split("\\")[3]
-
     coords = path.name.split("_")[-1].split(".")[0]
+
     if coords.startswith("Z"):
         print(path.absolute().__str__() + "|" + code + " " + year + " " + coords)
     else:
