263a2aff905c4e81ae5fbc299ff39d6a2b8d0bb3
Martin Eriksen <m@rtin.bz>
2023-01-26 09:01:23 -0300

    .

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 from pathlib import Path
+from lokaliteter import lokaliteter as lokalitet
 
 path = "L:/RASTER_DATA/"
 ext = "*.ecw"
 
 for path in Path(path).rglob(ext):
     code = path.absolute().__str__().split("\\")[2]
+
+    code = code.split()[0]  # fjern ting i ()
+
     year = path.absolute().__str__().split("\\")[3]
     coords = path.name.split("_")[-1].split(".")[0]
 
-    if coords.startswith("Z"):
-        print(path.absolute().__str__() + "|" + code + " " + year + " " + coords)
-    else:
-        print(path.absolute().__str__() + "|" + code + " " + year)
+    if not year[0].isdigit():
+        year = "ukendt årstal"
 
+    if coords.startswith("Z"):
+        print(path.absolute().__str__() + "|" + lokalitet[code] + " " + year + " " + coords)
+    else:
+        print(path.absolute().__str__() + "|" + lokalitet[code] + " " + year)
